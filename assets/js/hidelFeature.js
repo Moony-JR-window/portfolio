e63902ee8d3a4e769152ea94971f24a0b69251f2
mony.rorn <mony.rorn@wingbank.com.kh>
2025-04-29 21:41:24 +0700

    hide@1.0.0

---
+++ assets/js/hidelFeature.js
@@ -0,0 +1,18 @@
+
+function togglePhone() {
+    const phone = document.getElementById('phone-number');
+    phone.classList.toggle('blurred');
+    phone.classList.toggle('visible');
+  }
+
+//   function toggleBlur(element) {
+//     element.classList.toggle('visible');
+//   }
+
+function toggleBlur(wrapper) {
+    wrapper.classList.toggle('visible');
+  }
+
+  function hbd(wrapper) {
+    wrapper.classList.toggle('visible');
+  }
